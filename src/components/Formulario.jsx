a0b6e76e3de07f5635c31fdedccdef07e0a47a8e
sheyla astorga <sheylaastorga1998@gmail.com>
2024-02-02 19:06:42 -0300

    cree os estados para manejar los valores de los campos nombre, apellido, dni y email y agregue la funcion handleSubmit que se ejecutara cuando se envie el formulario e onChange para actualizar los estados dde los estados cuando el usuario ingrese sus datos

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,26 +1,64 @@
-import Form from 'react-bootstrap/Form';
-import { Button } from 'react-bootstrap';
-
+import Form from "react-bootstrap/Form";
+import { useState } from "react";
+import { Button } from "react-bootstrap";
 
 const Formulario = () => {
+  const [nombre, setNombre] = useState("");
+  const [apellido, setApellido] = useState("");
+  const [dni, setDni] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>
-      <Form>
+    <section>
+      <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
           <Form.Label>Nombre</Form.Label>
-          <Form.Control type="text" placeholder="Ingrese su nombre"  minLength={4} maxLength={20}/>
+          <Form.Control
+            type="text"
+            placeholder="Ingrese su nombre"
+            minLength={4}
+            maxLength={20}
+            value={nombre}
+            onChange={(e)=> setNombre(e.target.value)}
+          />
           <Form.Label>Apellido</Form.Label>
-          <Form.Control type="text" placeholder="Ingrese su apellido"  minLength={4} maxLength={20} />
+          <Form.Control
+            type="text"
+            placeholder="Ingrese su apellido"
+            minLength={4}
+            maxLength={20}
+            value={apellido}
+            onChange={(e)=> setApellido(e.target.value)}
+          />
           <Form.Label>DNI</Form.Label>
-          <Form.Control type="number" placeholder="Ingrese su DNI" maxLength={8}/>
+          <Form.Control
+            type="number"
+            placeholder="Ingrese su DNI"
+            maxLength={8}
+            value={dni}
+            onChange={(e)=> setDni(e.target.value)}
+          />
           <Form.Label>Email</Form.Label>
-          <Form.Control type="email" placeholder="correo@correo.com" min={15} maxLength={30}/>
+          <Form.Control
+            type="email"
+            placeholder="correo@correo.com"
+            min={15}
+            maxLength={30}
+            value={email}
+            onChange={(e)=> setEmail(e.target.value)}
+          />
         </Form.Group>
         <div className="text-center">
-          <Button variant="dark" type='submit'>Enviar</Button>
+          <Button variant="dark" type="submit">
+            Enviar
+          </Button>
         </div>
       </Form>
-    </div>
+    </section>
   );
 };
 
